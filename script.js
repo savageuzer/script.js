43abc22e94844654c47296db91083b83fcd96067
uzerbagh <uzer.y.bagh@gmail.com>
2023-09-17 23:00:36 +0530

    update

---
+++ script.js
@@ -0,0 +1,47 @@
+// console.log('9-sum-of-multiples');
+// function sumOfMultiplesValue(curLimits) {
+
+//     let sumOfMultipleValue = 0;
+
+//     for(let i =0; i<= curLimits; i++) {
+//         if (i%3 ===0 || i % 5 ===0) {
+//             sumOfMultipleValue +=1;
+
+//         }
+//     }
+//     console.log(`sumOfMultipleValue of 3 & 5 upto ${curLimit} digit is:`,sumOfMultipleValue)
+// }
+
+// sumOfMultiplesValue(10);
+
+
+// var changeText=document.getElementById('head');
+// console.log(head);
+// head.textContent = 'hell'
+
+
+// var card = document.getElementById('card')
+// console.log(card);
+
+// function doAction(val){
+//     document.getElementById('card').innerText="uzer";
+//     card.style.background="yellow";
+
+// }
+
+
+// function myFunction() {
+//   document.getElementById("demo").innerHTML = "I have changed!";
+// }
+
+
+// var head = document.getElementById('changeText')
+// console.log(head);
+
+function doAction(value){
+    // alert("Hi");
+    document.getElementById('demo').innerText = "uzer is my name and that is enough ";
+    const image = document.getElementById('cardImage');
+    image.src='\client3.jpg'
+    // head.style.background="pink";
+}
