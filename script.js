84bb22f5bcab5d358a7f07b8be16188f86e746df
uzerbagh <uzer.y.bagh@gmail.com>
2023-12-20 13:57:08 +0530

    new js

--- script.js
+++ script.js
@@ -38,10 +38,91 @@
 // var head = document.getElementById('changeText')
 // console.log(head);
 
-function doAction(value){
+// function doAction(value){
     // alert("Hi");
-    document.getElementById('demo').innerText = "uzer is my name and that is enough ";
-    const image = document.getElementById('cardImage');
-    image.src='\client3.jpg'
+    // document.getElementById('demo').innerText = "uzer is my name and that is enough ";
+    // const image = document.getElementById('cardImage');
+    // image.src='\client3.jpg'
     // head.style.background="pink";
-}
+// }
+
+
+// let promise = new Promise(function(resolve,reject){
+//     setTimeout(() => reject("Sorry"),1000);
+// });
+// promise.then(
+//     result => alert(result),
+//     error => alert(error)
+// );
+
+// const students = [
+    // {name: "azam", subject  }    
+// ]
+
+// function isPalindrome(str){
+//     let j = str,2
+//     for(lenght()/2; i++) {
+//         if (str[i] != str[j]){
+//             return false;
+//         }
+//         j--;
+//     }
+//     let str1 = "racecar";
+//     let str2 = "nitin";
+//     let str3 = "rama";
+
+// console.log(isPalindrome(str1));
+// console.log(isPalindrome(str2));
+// console.log(isPalindrome(str3));
+// }
+
+// var arr = ["india", "usa", "japan", "korea"];
+// var arr2  = new Array("jaipur", "mumbai", "pune", "imagica");
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+// arr.push("palestine");
+// console.log(arr);
+
+// arr.unshift("turkey");
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// console.log(arr.indexOf("japan"));
+
+// console.log(Array.from([2,3,4], (x) => x * x ));
+
+// var isOdd = (no) => {
+//     console.log(no)
+//     return no % 2 != 0;
+// };
+
+// var array123 =[1, 3, 5, 9];
+// var result = array123.every(isOdd);
+// console.log("Result" + result);
+
+// var isEven = (no) => {
+//     console.log(no)
+//     return no % 2 != 0;
+// };
+
+// var array123 =[1, 3, 5, 9];
+// var result = array123.every(isEven);
+// console.log("Result" + result);
+
+// var array1 = [5, 10, 50, 500];
+// console.log(array1.every((x) => x % 5 === 0));
+ 
+var isMultipeof5 = (no) => {
+        console.log(no)
+        return no % 5 != 0;
+    };
+    
+    var array123 =[5, 10, 15, 20];
+    var result = array123.every(isMultipeof5);
+    console.log("Result" + result);
+    
